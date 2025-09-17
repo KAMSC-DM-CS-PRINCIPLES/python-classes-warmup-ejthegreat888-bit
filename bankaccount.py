70edeb27d247771760f5f575da606be29c2bc782
ejthegreat888-bit <ej.the.great888@gmail.com>
2025-09-17 13:41:46 -0400

    Update bankaccount.py

--- bankaccount.py
+++ bankaccount.py
@@ -2,7 +2,25 @@
 
 class BankAccount:
 
+    def __init__(self, balance):
+        self.balance = balance
 
-    if __name__ == "__main__":
+    def deposite(self, n):
+        if self.balance < 0:
+            return self.balance
+        self.balance += n
+        return self.balance
+
+    def withdrawl(self, n):
+        if n > self.balance:
+            return "Insufficient Funds"
+        elif n < 0:
+            return self.balance
+        return self.balance - n
+
+    def get_balance(self):
+        return self.balance
+
+if __name__ == "__main__":
     # create BankAccount below this
-        pass
+    pass
